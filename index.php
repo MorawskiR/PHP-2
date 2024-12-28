be9333cc7a25fc2b377160ca292dfdd69facb27a
Rafa <r.morawski92@gmail.com>
2024-12-28 23:11:43 +0100

    draft

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title> osadnicy gra przeglądarkowa</title>
+</head>
+
+<body>
+
+Tylko martwi <br /> <br />
+
+<form action="zaloguj.php" method="post">
+Login: <br /> <input type="text" name="login" /> <br /> <br />
+Hasło: <br /> <input type="password" name="haslo" /> <br /> <br />
+<input type="submit" value="Zaloguj sie" />
+</form>
+</body>
+</html>
